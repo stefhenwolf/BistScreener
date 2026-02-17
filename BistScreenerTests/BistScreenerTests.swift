89134120bf5e4e4e6480fdeabcddaf010bc48948
Adınız <email@adresiniz.com>
2026-02-18 02:28:12 +0300

    Initial Commit

---
+++ BistScreenerTests/BistScreenerTests.swift
@@ -0,0 +1,17 @@
+//
+//  BistScreenerTests.swift
+//  BistScreenerTests
+//
+//  Created by Sedat Pala on 18.02.2026.
+//
+
+import Testing
+@testable import BistScreener
+
+struct BistScreenerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ BistScreenerUITests/BistScreenerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  BistScreenerUITestsLaunchTests.swift
+//  BistScreenerUITests
+//
+//  Created by Sedat Pala on 18.02.2026.
+//
+
+import XCTest
+
+final class BistScreenerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
